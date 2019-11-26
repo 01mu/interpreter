9b1b359dfada41ef21a240b8711a3a6cc93360de
daniel <developmentleviathan@gmail.com>
2019-11-26 18:58:23 -0500

    parser

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -12,9 +12,11 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
     scanner := bufio.NewScanner(in)
+
     for {
         fmt.Printf(PROMPT)
         scanned := scanner.Scan()
+
         if !scanned {
             return
         }
