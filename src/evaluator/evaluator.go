7b470695adca178d4a37f104c905efdcfdc5145e
daniel <developmentleviathan@gmail.com>
2019-12-11 14:32:06 -0500

    infix expressions

---
+++ src/evaluator/evaluator.go
@@ -0,0 +1,127 @@
+package evaluator
+
+import (
+    "ast"
+    "object"
+)
+
+func Eval(node ast.Node) object.Object {
+    switch node := node.(type) {
+        case *ast.Program:
+            return evalStatements(node.Statements)
+        case *ast.ExpressionStatement:
+            return Eval(node.Expression)
+        case *ast.IntegerLiteral:
+            return &object.Integer{Value: node.Value}
+        case *ast.Boolean:
+            return nativeBoolToBooleanObject(node.Value)
+        case *ast.PrefixExpression:
+            right := Eval(node.Right)
+            return evalPrefixExpression(node.Operator, right)
+        case *ast.InfixExpression:
+            left := Eval(node.Left)
+            right := Eval(node.Right)
+            return evalInfixExpression(node.Operator, left, right)
+    }
+
+    return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+    var result object.Object
+
+    for _, statement := range stmts {
+        result = Eval(statement)
+    }
+
+    return result
+}
+
+var (
+    TRUE = &object.Boolean{Value: true}
+    FALSE = &object.Boolean{Value: false}
+    NULL = &object.Null{}
+)
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+    if input {
+        return TRUE
+    }
+
+    return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+    switch operator {
+        case "!":
+            return evalBangOperatorExpression(right)
+        case "-":
+            return evalMinusPrefixOperatorExpression(right)
+        default:
+            return NULL
+    }
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+    switch right {
+        case TRUE:
+            return FALSE
+        case FALSE:
+            return TRUE
+        case NULL:
+            return TRUE
+        default:
+            return FALSE
+    }
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+    if right.Type() != object.INTEGER_OBJ {
+        return NULL
+    }
+
+    value := right.(*object.Integer).Value
+
+    return &object.Integer{Value: -value}
+}
+
+func evalInfixExpression(operator string, left, right object.Object,
+    ) object.Object {
+    switch {
+        case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+            return evalIntegerInfixExpression(operator, left, right)
+        case operator == "==":
+            return nativeBoolToBooleanObject(left == right)
+        case operator == "!=":
+            return nativeBoolToBooleanObject(left != right)
+        default:
+            return NULL
+    }
+}
+
+func evalIntegerInfixExpression(operator string, left, right object.Object,
+    ) object.Object {
+    leftVal := left.(*object.Integer).Value
+    rightVal := right.(*object.Integer).Value
+
+    switch operator {
+        case "+":
+            return &object.Integer{Value: leftVal + rightVal}
+        case "-":
+            return &object.Integer{Value: leftVal - rightVal}
+        case "*":
+            return &object.Integer{Value: leftVal * rightVal}
+        case "/":
+            return &object.Integer{Value: leftVal / rightVal}
+        case "<":
+            return nativeBoolToBooleanObject(leftVal < rightVal)
+        case ">":
+            return nativeBoolToBooleanObject(leftVal > rightVal)
+        case "==":
+            return nativeBoolToBooleanObject(leftVal == rightVal)
+        case "!=":
+            return nativeBoolToBooleanObject(leftVal != rightVal)
+        default:
+            return NULL
+    }
+}
--- src/repl/repl.go
+++ src/repl/repl.go
@@ -6,6 +6,7 @@ import (
     "io"
     "lexer"
     "parser"
+    "evaluator"
 )
 
 const PROMPT = ">> "
@@ -25,13 +26,18 @@ func Start(in io.Reader, out io.Writer) {
         p := parser.New(l)
 
         program := p.ParseProgram()
+
         if len(p.Errors()) != 0 {
             printParserErrors(out, p.Errors())
             continue
         }
 
-        io.WriteString(out, program.String())
-        io.WriteString(out, "\n")
+        evaluated := evaluator.Eval(program)
+
+        if evaluated != nil {
+            io.WriteString(out, evaluated.Inspect())
+            io.WriteString(out, "\n")
+        }
     }
 }
 
