dd2bae0332056f45ba90eca7a54b4d788ff8551c
daniel <developmentleviathan@gmail.com>
2020-10-24 18:04:12 -0400

    create tests for instruction to string conv

--- core/compiler.c
+++ core/compiler.c
@@ -42,7 +42,6 @@ typedef struct {
     int ic;
 } CompilerTest;
 
-
 Compiler * compiler_new() {
     Compiler * compiler = malloc(sizeof(Compiler));
 
@@ -53,7 +52,7 @@ Compiler * compiler_new() {
     return compiler;
 }
 
-Bytecode * bytecode(Compiler * c) {
+Bytecode * compiler_get_bytecode(Compiler * c) {
     Bytecode * bc = malloc(sizeof(Bytecode));
 
     bc->instructions = c->instructions;
@@ -63,8 +62,8 @@ Bytecode * bytecode(Compiler * c) {
     return bc;
 }
 
-void compile(Program * program) {
-
+void * compile(Program * program) {
+    return NULL;
 }
 
 HashMap * definitions;
@@ -75,6 +74,10 @@ void init_definitions() {
 
 void free_definitions() {
     hash_map_free(definitions);
+}
+
+byte new_op() {
+    return op_count++;
 }
 
 char * new_op_constant() {
@@ -124,14 +127,19 @@ byte * encode_big_endian(int num, byte * arr, int offset) {
     return arr;
 }
 
-Instruction * make(byte op, int * operands) {
+Instruction * make(byte op, int operands[]) {
     SortedList * sl = hash_map_find(definitions, op_code_to_str(op));
-    int oc = sizeof(operands)/sizeof(operands[0]);
-    int instruction_len = 1;
 
     if(sl == NULL) {
-        return NULL;
+        Instruction * inst = malloc(sizeof(Instruction));
+        inst->instruction = NULL;
+        inst->instruction_length = 0;
+
+        return inst;
     }
+
+    int oc = sizeof(*operands)/sizeof(operands[0]);
+    int instruction_len = 1;
 
     Definition * def = sl->data;
 
@@ -157,6 +165,8 @@ Instruction * make(byte op, int * operands) {
     Instruction * inst = malloc(sizeof(Instruction));
     inst->instruction = instruction;
     inst->instruction_length = offset;
+
+    //printf("%i\n",  inst->instruction_length);
 
     return inst;
 }
@@ -189,32 +199,70 @@ Program * comp_test_parse(char * input) {
     return parse_program(parser);
 }
 
-Instruction * concat_instructions(Instruction ** expected_instructions, int ic) {
-    for(int i = 0; i < ic; i++) {
-        for(int j = 0; j < expected_instructions[i]->instruction_length; j++) {
-            printf("%i\n", expected_instructions[i]->instruction[j]);
+// =============================================================================
+// test_integer_arithmetic
+// =============================================================================
+
+void concat_instructions(byte * concat, Instruction ** ei, int ic) {
+    int i, j, b;
+
+    for(i = 0, b = 0; i < ic; i++) {
+        for(j = 0; j < ei[i]->instruction_length; j++, b++) {
+            concat[b] = ei[i]->instruction[j];
         }
     }
 }
 
-void test_instructions(Instruction ** expected_instructions, int ic,
-    Instruction * instructions) {
+bool comp_test_integer_object(Object * obj, int test) {
+    IntegerObject * v = obj->value;
+    return v->value == test;
+}
 
-    Instruction * concat = concat_instructions(expected_instructions, ic);
+void test_constants(int * ec, int cc, Object ** actual, int ac) {
+    int i;
 
+    for(i = 0; i < cc; i++) {
+        printf("%i %i\n", ec[i], comp_test_integer_object(actual[i], ec[i]));
+    }
+}
 
+void test_instructions(Instruction ** ei, int ic, Instruction * instructions) {
+    int i, sz = 0;
+    byte * concat = NULL;
+
+    for(i = 0; i < ic; i++) {
+        sz += ei[i]->instruction_length;
+    }
+
+    concat = malloc(sizeof(byte) * sz);
+    concat_instructions(concat, ei, ic);
+
+    //printf("%i %i\n", sz, instructions->instruction_length);
 }
 
 void run_compiler_tests(CompilerTest ** tests, int tc, int z[]) {
     int i;
+    Program * program = NULL;
+    Compiler * compiler = NULL;
+    Bytecode * bytecode = NULL;
 
     for(i = 0; i < tc; i++) {
-        Program * program = comp_test_parse(tests[i]->input);
-        Compiler * compiler = compiler_new();
-        Bytecode * bc = bytecode(compiler);
+        program = comp_test_parse(tests[i]->input);
+        compiler = compiler_new();
 
-        test_instructions(tests[i]->expected_instructions, tests[i]->ic, bc->instructions);
+        if(compile(program) != NULL) {
+            break;
+        }
 
+        bytecode = compiler_get_bytecode(compiler);
+
+        test_instructions(tests[i]->expected_instructions, tests[i]->ic,
+            bytecode->instructions);
+
+        break;
+
+        test_constants(tests[i]->expected_constants, tests[i]->cc,
+            bytecode->constants, bytecode->constant_count);
     }
 }
 
@@ -222,18 +270,17 @@ void test_integer_arithmetic() {
     CompilerTest ** tests = malloc(sizeof(CompilerTest *));
 
     CompilerTest * test = malloc(sizeof(CompilerTest));
-    int * a = malloc(sizeof(int)), * b = malloc(sizeof(int));
+    //int * a = malloc(sizeof(int)), * b = malloc(sizeof(int));
 
     test->input = malloc(sizeof(char) * 10);
     sprintf(test->input, "%s", "1 + 2");
 
     test->expected_constants = malloc(sizeof(int) * 2);
     test->expected_constants[0] = 1;
-    test->expected_constants[0] = 2;
+    test->expected_constants[1] = 2;
     test->expected_instructions = malloc(sizeof(byte *) * 2);
 
-    a[0] = 0;
-    b[0] = 1;
+    int a[1] = {0}, b[1] = {1};
     test->expected_instructions[0] = make(0, a);
     test->expected_instructions[1] = make(0, b);
 
@@ -247,19 +294,94 @@ void test_integer_arithmetic() {
     run_compiler_tests(tests, 1, v);
 }
 
+// =============================================================================
+// test_instruction_string
+// =============================================================================
+
+void test_instruction_string() {
+    Instruction ** instructions = malloc(sizeof(Instruction *) * 3);
+    int a[1] = {1}, b[1] = {2}, c[1] = {65535};
+
+    instructions[0] = make(0, a);
+    instructions[1] = make(1, b);
+    instructions[2] = make(2, c);
+
+    byte * concat = NULL;
+    int i, sz = 0;
+
+    for(i = 0; i < 3; i++) {
+        sz += instructions[i]->instruction_length;
+    }
+
+
+    //concat = malloc(sizeof(byte) * sz);
+
+    //concat_instructions(concat, instructions, 3);
+
+}
+
+// =============================================================================
+// test_read_operands
+// =============================================================================
+
+void read_operands(Definition * def, Instruction * ins) {
+    int i;
+    int offset = 1;
+
+    for(i = 0; i < def->wc; i++) {
+        int width = def->widths[i];
+
+        switch(width) {
+            case 2:
+            for(int j = 0; j < ins->instruction_length; j++) {
+                printf("%i\n", ins->instruction[j]);
+            }
+
+            //case 2: encode_big_endian(operands[i], instruction, offset); break;
+        }
+
+        offset += width;
+    }
+}
+
+
+void test_read_operands() {
+    int a[1] = {65534};
+    int c = 1, i;
+    struct {
+        byte opcode;
+        int * operands;
+        int bytes_read;
+    } t[1] = {0, a, 2};
+
+    for(i = 0; i < c; i++) {
+        Instruction * instruction = make(t[i].opcode, t[i].operands);
+        Definition * def = lookup_op(t[i].opcode);
+
+        read_operands(def, instruction);
+    }
+}
+
+
+// =============================================================================
+
+// =============================================================================
+
 void comp_test() {
     init_definitions();
     create_new_definition();
+    create_new_definition();
+    create_new_definition();
     SortedList * m = hash_map_find(definitions, "0");
-    printf("%p\n", m);
-
+    //printf("%p\n", m);
     byte n = 0;
-    printf("%i\n", n);
 
     int op[1] = {65534};
-    Instruction * new_inst = make(n, op);
+    //Instruction * new_inst = make(n, op);
     //printf("%li\n", sizeof(new_inst) / sizeof(new_inst[0]));
     //printf("%i\n", new_inst->instruction[1]);
 
-    test_integer_arithmetic();
+    //test_integer_arithmetic();
+    //test_instruction_string();
+    test_read_operands();
 }
